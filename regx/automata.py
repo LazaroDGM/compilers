ea6eb8149a58cac52a149cfba30bf68a1c9880f9
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-01-11 15:29:21 -0500

    add Goto & epsilonClosure

--- regx/automata.py
+++ regx/automata.py
@@ -56,3 +56,56 @@ class DFA(NFA):
         if self.current in self.finals:
             return True
         return False
+
+def move(automaton, states, symbol):
+    '''
+    Dado un conjunto de estados Q' y un simbolo se computan todos los posibles
+    nodos a los que se puede llegar desde los estados de Q', utilizando al simbolo
+    en cuestion como paso de transicion
+
+    Sea Q' subconjunto de los estados Q:
+    GOTO(Q', c) = {q_j de Q | q_i en Q', q_j este en t(q_i, c)}
+
+    Parametros
+    ------------
+    `automaton`: Automata
+    `states`: Conjunto de estados
+    `symbol`: Simbolo de transicion
+
+    Return
+    -----------
+    `new_states`: Conjunto de estados de `automaton` resultantes
+    '''
+    moves = set()
+    for state in states:
+        # Your code here
+        next_states = automaton.transitions[state].get(symbol, [])
+        moves.update(next_states)
+    return moves
+
+def epsilon_closure(automaton, states):
+    '''
+    Funcion recursiva que calcula al epsilon-Clasura de un conjunto de estados
+    en un automata. Este nuevo conjunto esta formado por todos los estados del
+    conjunto actual, mas los estados a los que se puede llegar haciendo tantas
+    transiciones con epsilon como sean posibles.
+
+    Parametros
+    ------------
+    `automaton`: Automata
+    `states`: Conjunto de estados
+
+    Return
+    -----------
+    `new_states`: Conjunto de estados de `automaton` resultantes
+    '''    
+    pending = [ s for s in states ] # equivalente a list(states) pero me gusta así :p
+    closure = { s for s in states } # equivalente a  set(states) pero me gusta así :p
+    
+    while pending:
+        state = pending.pop()
+        # Your code here
+        news_states = automaton.transitions[state].get('', [])        
+        closure.update(epsilon_closure(automaton, news_states))
+                
+    return set(closure)
