f45810f7b1bbc353fb2b8422b4dc32ab94b3b449
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-01-12 10:18:08 -0500

    add GrammarRegexSimple

--- regx/regx.py
+++ regx/regx.py
@@ -1,5 +1,5 @@
 from automata import NFA, automata_union, automata_concatenation, automata_closure, automata_positive_closure
-
+from pycompiler import Grammar
 ########## Clases Base para los nodos del AST ##########
 
 class Node:
@@ -36,7 +36,8 @@ class BinaryNode(Node):
     def operate(lvalue, rvalue):
         raise NotImplementedError()
 
-######### Nodes Automaton AST ############
+######### Nodos Automata AST ############
+
 EPSILON = 'ε'
 
 class EpsilonNode(AtomicNode):
@@ -83,3 +84,57 @@ class ConcatNode(BinaryNode):
     def operate(lvalue, rvalue):        
         concat = automata_concatenation(lvalue, rvalue)
         return concat
+
+############### Gramatica de REGEX ####################
+
+def GrammarRegexSimple():
+    '''
+    Generador de la gramatica LL(1) para expresiones regulares simples
+    Solo implementa Union, Concatenacion y Clausura
+    
+    La gramatica es:
+
+    E   -> TX
+    X   -> |E
+        -> epsilon
+    T   -> FY
+    Y   -> T
+        -> epsilon
+    F   -> AZ
+    Z   -> *
+        -> epsilon
+    A   -> t
+        -> (E)
+        -> ε
+
+    Retorna:
+    --------------
+    `G`: Gramatica REGEX Simple (instancia de `Grammer`)
+    '''
+    G = Grammar()
+
+    E = G.NonTerminal('E', True)
+    T, F, A, X, Y, Z = G.NonTerminals('T F A X Y Z')
+    pipe, star, opar, cpar, symbol, epsilon = G.Terminals('| * ( ) symbol ε')
+
+    E %= T + X, lambda h,s: s[2], None, lambda h,s: s[1]
+
+    X %= pipe + E, lambda h,s: UnionNode(h[0], s[2]), None, None
+    X %= G.Epsilon, lambda h,s: h[0]#, None
+
+    T %= F + Y, lambda h,s: s[2], None, lambda h,s: s[1]
+
+    Y %= T, lambda h, s: ConcatNode(h[0], s[1]), None
+    Y %= G.Epsilon, lambda h,s: h[0]#, None
+
+    F %= A + Z, lambda h,s: s[2], None, lambda h,s: s[1]
+
+    Z %= star, lambda h,s: ClosureNode(h[0]), None
+    Z %= G.Epsilon, lambda h,s: h[0]
+
+    A %= symbol, lambda h,s: SymbolNode(s[1]), None
+    A %= opar + E + cpar, lambda h,s: s[2], None, None, None
+    A %= epsilon, lambda h,s: EpsilonNode(EPSILON), None
+
+    return G
+
