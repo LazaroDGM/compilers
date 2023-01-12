b44cec42143eae3cb50cb2126205af09f1536a04
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-01-12 09:59:09 -0500

    add nodes automaton

--- regx/regx.py
+++ regx/regx.py
@@ -1,3 +1,4 @@
+from automata import NFA, automata_union, automata_concatenation, automata_closure, automata_positive_closure
 
 ########## Clases Base para los nodos del AST ##########
 
@@ -35,3 +36,50 @@ class BinaryNode(Node):
     def operate(lvalue, rvalue):
         raise NotImplementedError()
 
+######### Nodes Automaton AST ############
+EPSILON = 'ε'
+
+class EpsilonNode(AtomicNode):
+    def evaluate(self):   
+        nfa = NFA(states=2,
+            finals=[1],
+            transitions={
+                (0, ''): [1],
+            },
+            start=0)
+        return nfa
+
+class SymbolNode(AtomicNode):
+    def evaluate(self):
+        s = self.lex
+        nfa = NFA(states=2,
+            finals=[1],
+            transitions={
+                (0, s): [1],
+            },
+            start=0)
+        return nfa   
+
+class ClosureNode(UnaryNode):
+    @staticmethod
+    def operate(value):
+        closure = automata_closure(value)
+        return closure
+
+class PositiveClosureNode(UnaryNode):
+    @staticmethod
+    def operate(value):                
+        closure = automata_positive_closure(value)
+        return closure
+
+class UnionNode(BinaryNode):
+    @staticmethod
+    def operate(lvalue, rvalue):                
+        union = automata_union(lvalue, rvalue)
+        return union
+
+class ConcatNode(BinaryNode):
+    @staticmethod
+    def operate(lvalue, rvalue):        
+        concat = automata_concatenation(lvalue, rvalue)
+        return concat
