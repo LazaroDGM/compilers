e5cf3b9553676a04a7b51937f36545a9b2dce879
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-01-11 23:16:02 -0500

    add ContainerSet

--- regx/pycompiler.py
+++ regx/pycompiler.py
@@ -510,3 +510,45 @@ class Item:
 
     def Center(self):
         return Item(self.production, self.pos)
+
+class ContainerSet:
+    def __init__(self, *values, contains_epsilon=False):
+        self.set = set(values)
+        self.contains_epsilon = contains_epsilon
+        
+    def add(self, value):
+        n = len(self.set)
+        self.set.add(value)
+        return n != len(self.set)
+        
+        
+    def set_epsilon(self, value=True):
+        last = self.contains_epsilon
+        self.contains_epsilon = value
+        return last != self.contains_epsilon
+        
+    def update(self, other):
+        n = len(self.set)
+        self.set.update(other.set)
+        return n != len(self.set)
+    
+    def epsilon_update(self, other):
+        return self.set_epsilon(self.contains_epsilon | other.contains_epsilon)
+    
+    def hard_update(self, other):
+        return self.update(other) | self.epsilon_update(other)
+    
+    def __len__(self):
+        return len(self.set) + int(self.contains_epsilon)
+    
+    def __str__(self):
+        return '%s-%s' % (str(self.set), self.contains_epsilon)
+    
+    def __repr__(self):
+        return str(self)
+    
+    def __iter__(self):
+        return iter(self.set)
+    
+    def __eq__(self, other):
+        return isinstance(other, ContainerSet) and self.set == other.set and self.contains_epsilon == other.contains_epsilon
