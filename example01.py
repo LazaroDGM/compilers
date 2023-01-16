b945a68e1f65b6aaff8e864d84ed47a409684a57
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-01-16 15:21:50 -0500

    add grammar LL(1)

--- example01.py
+++ example01.py
@@ -55,3 +55,33 @@ class PowNode(BinaryNode):
     def operate(lvalue, rvalue):
         lvalue ** rvalue
 
+########################################################
+#                 GRAMATICA 01  LL(1)                  #
+#                     ATRIBUTADA                       #
+########################################################
+
+G = Grammar()
+E = G.NonTerminal('E', True)
+T, F, X, Y = G.NonTerminals('T F X Y')
+plus, minus, star, div, opar, cpar, operpow, num = G.Terminals('+ - * / ( ) ^ num')
+
+
+E %= T + X, lambda h,s: s[2], None, lambda h,s: s[1]
+
+X %= plus + T + X, lambda h, s: s[3], None, None, lambda h,s: PlusNode(h[0], s[2])
+X %= minus + T + X, lambda h, s: s[3], None, None, lambda h,s: MinusNode(h[0], s[2])
+X %= G.Epsilon, lambda h, s: h[0]
+
+T %= F + Y, lambda h, s: s[2], None, lambda h, s: s[1]
+
+Y %= star + F + Y, lambda h,s: s[3], None, None, lambda h, s: StarNode(h[0], s[2])
+Y %= div + F + Y, lambda h,s: s[3], None, None, lambda h, s: DivNode(h[0], s[2])
+Y %= G.Epsilon, lambda h, s: h[0]
+
+F %= num, lambda h, s: ConstantNumberNode(s[1]), None
+F %= opar + E + cpar, lambda h, s: s[2], None, None, None
+
+########################################################
+# ==================================================== #
+########################################################
+
