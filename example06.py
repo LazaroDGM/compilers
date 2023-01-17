7c4f6956a7d1eb73dfa19340c356c4adf709b30d
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-01-17 16:44:00 -0500

    add utils06

--- example06.py
+++ example06.py
@@ -12,11 +12,11 @@ class Language06:
 
         program = G.NonTerminal('<program>', startSymbol=True)
         stat_list, stat = G.NonTerminals('<stat_list> <stat>')
-        let_var, def_func, arg_list, let_const = G.NonTerminals('<let-var> <def-func> <arg-list> <let-const>')
+        let_var, def_func, param_list, let_const = G.NonTerminals('<let-var> <def-func> <arg-list> <let-const>')
         expr, term, factor, atom = G.NonTerminals('<expr> <term> <factor> <atom>')
-        func_call, param_list, param = G.NonTerminals('<func-call> <param-list> <param>')
+        func_call, arg_list, arg = G.NonTerminals('<func-call> <param-list> <param>')
         if_stat, while_stat, asign_stat, return_stat = G.NonTerminals('<if-stat> <while-stat> <asign-stat> <return-const>')
-        arg, comp_list, comp = G.NonTerminals('<arg> <comp-list> <comp>')
+        param, comp_list, comp = G.NonTerminals('<arg> <comp-list> <comp>')
 
         var, func, const, returnx = G.Terminals('var func const return')
         semi, comma, opar, cpar, arrow, okey, ckey, colon = G.Terminals('; , ( ) -> { } :')
@@ -41,27 +41,27 @@ class Language06:
         stat %= def_func
         stat %= return_stat       
 
-        let_var %= var + typex + idx + asign + param
+        let_var %= var + typex + idx + asign + arg
         let_var %= var + typex + idx
 
-        let_const %= const + typex + idx + asign + param
+        let_const %= const + typex + idx + asign + arg
 
-        asign_stat %= idx + asign + param
+        asign_stat %= idx + asign + arg
 
-        def_func %= func + idx + opar + arg_list + cpar + arrow + typex + okey + stat_list + ckey
-        def_func %= func + idx + opar + arg_list + cpar + okey + stat_list + ckey
+        def_func %= func + idx + opar + param_list + cpar + arrow + typex + okey + stat_list + ckey
+        def_func %= func + idx + opar + param_list + cpar + okey + stat_list + ckey
 
-        return_stat %= returnx + param
+        return_stat %= returnx + arg
 
         if_stat %= ifx + opar + comp_list + cpar + colon + stat_list + endif
         if_stat %= ifx + opar + comp_list + cpar + colon + stat_list + elsex + stat_list + endif
 
         while_stat %= whilex + opar + comp_list + cpar + colon + stat_list + endwhile
 
-        arg_list %= arg
-        arg_list %= arg + comma + arg_list
+        param_list %= param
+        param_list %= param + comma + param_list
 
-        arg %= typex + idx
+        param %= typex + idx
 
         comp_list %= comp_list + andx + comp
         comp_list %= comp_list + orx + comp
@@ -93,13 +93,13 @@ class Language06:
         atom %= boolx
         atom %= func_call
 
-        func_call %= idx + opar + param_list + cpar
+        func_call %= idx + opar + arg_list + cpar
         func_call %= idx + opar + cpar
 
-        param_list %= param + comma + param_list
-        param_list %= param
+        arg_list %= arg + comma + arg_list
+        arg_list %= arg
         
-        param %= comp_list
+        arg %= comp_list
 
         nonzero_digits = '|'.join(str(n) for n in range(1,10))
         letters_lower = '|'.join(chr(n) for n in range(ord('a'),ord('z')+1))
@@ -177,3 +177,4 @@ class Language06:
         right_parse, operations = self.parser(tokens)  
         return True 
 
+L = Language06()
---
+++ utils06/utils.py
@@ -0,0 +1,101 @@
+try:
+    import visitor
+except:
+    from utils06 import visitor
+
+class Node:
+    def evaluate(self):
+        raise NotImplementedError()
+
+##################################
+class ProgramNode(Node):
+    def __init__(self, statements):
+        self.statements = statements
+        
+class StatementNode(Node):
+    pass
+
+#################################
+
+class VarDeclarationNode(StatementNode):
+    def __init__(self, idx, ttype, expr):        
+        self.id = idx
+        self.ttype= ttype
+        self.expr = expr
+
+class ConstDeclarationNode(StatementNode):
+    def __init__(self, idx, ttype, expr):        
+        self.id = idx
+        self.ttype= ttype
+        self.expr = expr
+
+class FuncDeclarationNode(StatementNode):
+    def __init__(self, idx, types, params, return_type, body):
+        self.id = idx
+        self.types= types
+        self.params = params
+        self.body = body
+
+class AsignVarNode(StatementNode):
+    def __init__(self, idx, expr) -> None:
+        self.id = idx
+        self.expr = expr
+
+class IfEndNode(StatementNode):
+    def __init__(self, cond, statements) -> None:        
+        self.cond = cond
+        self.statements = statements
+
+class IfElseEndNode(StatementNode):
+    def __init__(self, cond, statements_if, statements_else) -> None:        
+        self.cond = cond
+        self.statements_if = statements_if
+        self.statements_else = statements_else
+
+class IfElseEndNode(StatementNode):
+    def __init__(self, cond, statements) -> None:        
+        self.cond = cond
+        self.statements = statements  
+
+class ReturnNode(StatementNode):
+    def __init__(self, expr) -> None:        
+        self.expr = expr        
+
+##############################################
+
+class ExpressionNode(Node):
+    pass
+
+class AtomicNode(ExpressionNode):
+    def __init__(self, lex):
+        self.lex = lex        
+
+class BinaryNode(ExpressionNode):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+####################################
+
+class ConstantNumNode(AtomicNode):
+    pass
+
+class VariableNode(AtomicNode):
+    pass
+
+class CallNode(AtomicNode):
+    def __init__(self, idx, args):
+        AtomicNode.__init__(self, idx)
+        self.args = args
+
+class PlusNode(BinaryNode):
+    pass
+
+class MinusNode(BinaryNode):
+    pass
+
+class StarNode(BinaryNode):
+    pass
+
+class DivNode(BinaryNode):
+    pass
