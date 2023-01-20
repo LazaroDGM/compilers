6d5b1696425023a75b7ad0978f0a10744552e208
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-01-20 13:39:56 -0500

    add README

--- tools/regx.py
+++ tools/regx.py
@@ -124,10 +124,11 @@ def GrammarRegexSimple():
 
     E = G.NonTerminal('E', True)
     T, F, A, X, Y, Z = G.NonTerminals('T F A X Y Z')
-    pipe, star, opar, cpar, symbol, epsilon = G.Terminals('| * ( ) symbol ε')
+    pipe, star, opar, cpar, symbol, epsilon, plus = G.Terminals('| * ( ) symbol ε +')
 
     G._fixed_tokens = {
         '*': Token('*', star),
+        '+': Token('+', plus),
         '|': Token('|', pipe),
         '(': Token('(', opar),
         ')': Token(')', cpar),
@@ -148,6 +149,7 @@ def GrammarRegexSimple():
     F %= A + Z, lambda h,s: s[2], None, lambda h,s: s[1]
 
     Z %= star, lambda h,s: ClosureNode(h[0]), None
+    Z %= plus, lambda h,s: PositiveClosureNode(h[0]), None
     Z %= G.Epsilon, lambda h,s: h[0]
 
     A %= symbol, lambda h,s: SymbolNode(s[1]), None
